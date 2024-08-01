aca17250295b9e82400ee96b397c03b550f76805
William <william@wtdawson.info>
2024-08-01 06:49:08 +0100

    Some changes

--- NaughtsAndCrossesGenerator/Bot/BotInfo.xaml.cs
+++ NaughtsAndCrossesGenerator/Bot/BotInfo.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Ookii.Dialogs.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +24,43 @@ namespace NaughtsAndCrossesGenerator.Bot
         public BotInfo()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TaskDialog td = new TaskDialog()
+            {
+                WindowTitle = "Naughts and Crosses Bot",
+                Content = "Would you like to load an existing bot training file or create a new one and start from scratch?",
+                ButtonStyle = TaskDialogButtonStyle.CommandLinks
+            };
+
+            TaskDialogButton existing = new TaskDialogButton()
+            {
+                Text = "Existing training file"
+            };
+
+            TaskDialogButton create = new TaskDialogButton()
+            {
+                Text = "Create a new training file"
+            };
+
+            td.Buttons.Add(existing);
+            td.Buttons.Add(create);
+
+            TaskDialogButton result = td.ShowDialog();
+
+            if(result == existing)
+            {
+                VistaOpenFileDialog ofd = new VistaOpenFileDialog();
+                ofd.Filter = "*.json | (JSON File)";
+                ofd.ShowDialog();
+            } else if(result == create)
+            {
+                VistaSaveFileDialog sfd = new VistaSaveFileDialog();
+                sfd.Filter = "*.json | (JSON File)";
+                sfd.ShowDialog();
+            }
         }
     }
 }
